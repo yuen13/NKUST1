9daa8181259f090144f7a533e50223fb3ed6fe02
yuen13 <93572167+yuen13@users.noreply.github.com>
2021-11-16 16:30:42 +0800

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class prime {
+    public static void main(String[] args) {
+        int i , j;
+        for (i = 2; i <= 100; i++) {
+            for ( j = 2; j < i; j++) {
+                if (i % j == 0)
+                    break;
+
+            }
+            if (j >= i)
+                System.out.println(i);
+        }
+
+    }
+}
