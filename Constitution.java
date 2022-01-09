24241fa3d1f34f1f0e2b43e37df40310817a9282
ChiJui-c110181119 <93572490+ChiJui-c110181119@users.noreply.github.com>
2022-01-09 13:50:21 +0800

    Update Constitution.java

--- Constitution.java
+++ Constitution.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class Constitution {
     public static void main(String[] args) throws IOException {
         try {
-            Document doc = Jsoup.connect("http://www.ssu.org.tw/").get();
+            Document doc = Jsoup.connect("https://law.moj.gov.tw/").get();
             Elements news =doc.select("");
         } catch (Exception e) {
             System.out.println();
